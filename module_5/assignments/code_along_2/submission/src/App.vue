0c02458e290580fe97d5a2300041ce86062620d6
Sophie Crabtree <111188743+sophjvlia@users.noreply.github.com>
2023-04-16 18:40:52 +0800

    Add files via upload

---
+++ module_5/assignments/code_along_2/submission/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <AppContainer />
+</template>
+
+<script>
+import AppContainer from '@/components/AppContainer.vue'
+
+export default {
+  components: { AppContainer },
+  provide: {
+    inputStyle: {
+      "margin-left": "5px",
+      background: "lightgreen",
+      color: "blue",
+      border: "1px solid purple",
+      padding: "5px 10px",
+      "border-radius": "20px"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ module_5/assignments/code_along_2/submission/src/components/ProductInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <input type="text" v-model="message">
+  <button @click="emitData" :style="inputStyle">Emit data</button>
+</template>
+
+<script>
+export default {
+  emits: ['message'],
+  data() {
+    return {
+      message: 'Hello, there!'
+    }
+  },
+  methods: {
+    emitData() {
+      let message = this.message;
+      this.$emit('message', message)
+    }
+  },
+  inject: ["inputStyle"]
+}
+</script>
+
+<style>
+  input {
+    border-radius: 20px;
+    padding: 5px 10px;
+  }
+</style>
